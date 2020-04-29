939711d70abee0bcb382546faaab69bd1026d750
AVSheard <a_sheard@yahoo.co.uk>
2020-04-29 10:11:16 +0100

    Step one of order function complete

--- order.js
+++ order.js
@@ -1,11 +1,24 @@
 /** @format */
 
+let arr = [0, 1, 2];
+
 function setup() {
 	createCanvas(400, 300);
 }
 
 function draw() {
-	backgraound(0);
+	background(0);
+
+	let largeestX = -1;
+	for (let i = 0; i < arr.length - 1; i++) {
+		if (arr[i] < arr[i + 1]) {
+			largeestX = i;
+		}
+	}
+	if (largeestX === -1) {
+		noLoop();
+		console.log("Finished");
+	}
 }
 
 function swap(arr, index1, index2) {
