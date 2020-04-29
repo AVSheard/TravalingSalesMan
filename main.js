13475dbe88de087b51cad6da1b62e7a0e9e8fb7c
AVSheard <a_sheard@yahoo.co.uk>
2020-04-29 12:05:13 +0100

    added order function to main body of code

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 /** @format */
 
 let items = [];
-const totalItems = 10;
+const totalItems = 6;
 let bestDist;
 let bestRout;
+let curentOrder = [];
 
 function setup() {
 	createCanvas(400, 300);
 	for (let i = 0; i < totalItems; i++) {
 		items[i] = createVector(random(width), random(height));
+		curentOrder[i] = i;
 	}
 
 	bestDist = calcDist(items);
@@ -50,6 +52,8 @@ function draw() {
 		bestRout = [...items];
 		console.log(bestDist);
 	}
+
+	curentOrder = order(curentOrder);
 }
 
 function swap(arr, index1, index2) {
@@ -69,4 +73,33 @@ function calcDist(points) {
 		);
 	}
 	return totalDist;
+}
+
+function order(arr) {
+	let largestX = -1;
+	for (let i = 0; i < arr.length - 1; i++) {
+		if (arr[i] < arr[i + 1]) {
+			largestX = i;
+		}
+	}
+
+	let largestY = -1;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > arr[largestX]) {
+			largestY = i;
+		}
+	}
+
+	swap(arr, largestX, largestY);
+
+	let endArr = arr.splice(largestX + 1);
+	endArr.reverse();
+	arr = arr.concat(endArr);
+	console.log(arr);
+
+	if (largestX === -1) {
+		noLoop();
+		console.log("Finished");
+	}
+	return arr;
 }
--- order.js
+++ order.js
@@ -1,23 +1,11 @@
 /** @format */
 
-let arr = [0, 1, 2];
-
-function setup() {
-	createCanvas(400, 300);
-}
-
-function draw() {
-	background(0);
-
+function order() {
 	let largestX = -1;
 	for (let i = 0; i < arr.length - 1; i++) {
 		if (arr[i] < arr[i + 1]) {
 			largestX = i;
 		}
-	}
-	if (largestX === -1) {
-		noLoop();
-		console.log("Finished");
 	}
 
 	let largestY = -1;
@@ -28,10 +16,16 @@ function draw() {
 	}
 
 	swap(arr, largestX, largestY);
+
 	let endArr = arr.splice(largestX + 1);
 	endArr.reverse();
 	arr = arr.concat(endArr);
 	console.log(arr);
+
+	if (largestX === -1) {
+		noLoop();
+		console.log("Finished");
+	}
 }
 
 function swap(arr, index1, index2) {
@@ -39,3 +33,5 @@ function swap(arr, index1, index2) {
 	arr[index1] = arr[index2];
 	arr[index2] = temp;
 }
+
+module.exports = order;
